744e57ce8157b15a4be20681c3b4ac915d6123df
arthurnap24 <arthurnap24@gmail.com>
2022-11-06 07:39:07 -0500

    build exit codes

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
 main() {
-    ./build.sh
+    build_type=$1
     script_dir=$(dirname $0)
-    build_dir="${script_dir}/../build/"
+    test_dir="${script_dir}/../build/${build_type}/test/"
+    run_build $script_dir $build_type
+    run_tests $test_dir
+}
+
+run_build() {
+    script_dir=$1
+    build_type=$2
+    ./${script_dir}/build.sh $build_type
+    build_exit_code=$?
+    if [ $build_exit_code -ne 0 ]; then
+        echo "Non 0 exit code on build"
+        exit $build_exit_code
+    fi
+}
+
+run_tests() {
+    test_dir=$1
+    ./${test_dir}/game_test
 }
 
 print_usage() {
     echo -e """Usage:
-\test.sh [build-type]
+\ttest.sh [build-type]
 Supported build-type values:
 release - build release
 debug - build debug
