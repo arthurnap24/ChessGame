8c18afb927e199ed5dc31188f4ce62feffc8f922
arthurnap24 <arthurnap24@gmail.com>
2022-11-20 11:47:30 -0500

    clean

--- src/KingMovementSkill.h
+++ src/KingMovementSkill.h
@@ -10,8 +10,6 @@ public:
     bool move(const King& king, 
         const Coordinate& src,
         const Coordinate& dst);
-
-
 private:
     bool is_neighbor(const Coordinate& src, const Coordinate& dst);
 };
