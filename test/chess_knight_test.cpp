02ef33d97b8c3eada60fe10ea19e2439234a86e0
Arthur Napolitano <arthurnap24@gmail.com>
2023-02-23 00:16:42 +0000

    Better 'correct move' testing mechanism.

--- test/chess_knight_test.cpp
+++ test/chess_knight_test.cpp
@@ -10,100 +10,14 @@ using namespace ChessGame;
 
 using KnightTest = fixtures::ChessPieceMovementTest<Knight, KnightMovementSkill>;
 
-TEST_F(KnightTest, MoveUp)
+TEST_F(KnightTest, MoveVertical)
 {
-    Coordinate src{X::A, Y::ONE};
-    expect_move_bad(src, {X::A, Y::TWO});
-    expect_move_bad(src, {X::A, Y::THREE});
-    expect_move_bad(src, {X::A, Y::FOUR});
-    expect_move_bad(src, {X::A, Y::FIVE});
-    expect_move_bad(src, {X::A, Y::SIX});
-    expect_move_bad(src, {X::A, Y::SEVEN});
-    expect_move_bad(src, {X::A, Y::EIGHT});
+    expect_cannot_move_vertical();
 }
 
-TEST_F(KnightTest, MoveDown)
+TEST_F(KnightTest, MoveHorizontal)
 {
-    Coordinate src{X::A, Y::EIGHT};
-    expect_move_bad(src, {X::A, Y::SEVEN});
-    expect_move_bad(src, {X::A, Y::SIX});
-    expect_move_bad(src, {X::A, Y::FIVE});
-    expect_move_bad(src, {X::A, Y::FOUR});
-    expect_move_bad(src, {X::A, Y::THREE});
-    expect_move_bad(src, {X::A, Y::TWO});
-    expect_move_bad(src, {X::A, Y::ONE});
-}
-
-TEST_F(KnightTest, MoveLeft)
-{
-    Coordinate src{X::H, Y::ONE};
-    expect_move_bad(src, {X::G, Y::ONE});
-    expect_move_bad(src, {X::F, Y::ONE});
-    expect_move_bad(src, {X::E, Y::ONE});
-    expect_move_bad(src, {X::D, Y::ONE});
-    expect_move_bad(src, {X::C, Y::ONE});
-    expect_move_bad(src, {X::B, Y::ONE});
-    expect_move_bad(src, {X::A, Y::ONE});
-}
-
-TEST_F(KnightTest, MoveRight)
-{
-    Coordinate src{X::A, Y::ONE};
-    expect_move_bad(src, {X::B, Y::ONE});
-    expect_move_bad(src, {X::C, Y::ONE});
-    expect_move_bad(src, {X::D, Y::ONE});
-    expect_move_bad(src, {X::E, Y::ONE});
-    expect_move_bad(src, {X::F, Y::ONE});
-    expect_move_bad(src, {X::G, Y::ONE});
-    expect_move_bad(src, {X::H, Y::ONE});
-}
-
-TEST_F(KnightTest, MoveNE)
-{
-    Coordinate src{X::A, Y::ONE};
-    expect_move_bad(src, {X::B, Y::TWO});
-    expect_move_bad(src, {X::C, Y::THREE});
-    expect_move_bad(src, {X::D, Y::FOUR});
-    expect_move_bad(src, {X::E, Y::FIVE});
-    expect_move_bad(src, {X::F, Y::SIX});
-    expect_move_bad(src, {X::G, Y::SEVEN});
-    expect_move_bad(src, {X::H, Y::EIGHT});
-}
-
-TEST_F(KnightTest, MoveNW)
-{
-    Coordinate src{X::H, Y::ONE};
-    expect_move_bad(src, {X::G, Y::TWO});
-    expect_move_bad(src, {X::F, Y::THREE});
-    expect_move_bad(src, {X::E, Y::FOUR});
-    expect_move_bad(src, {X::D, Y::FIVE});
-    expect_move_bad(src, {X::C, Y::SIX});
-    expect_move_bad(src, {X::B, Y::SEVEN});
-    expect_move_bad(src, {X::A, Y::EIGHT});
-}
-
-TEST_F(KnightTest, MoveSE)
-{
-    Coordinate src{X::A, Y::EIGHT};
-    expect_move_bad(src, {X::B, Y::SEVEN});
-    expect_move_bad(src, {X::C, Y::SIX});
-    expect_move_bad(src, {X::D, Y::FIVE});
-    expect_move_bad(src, {X::E, Y::FOUR});
-    expect_move_bad(src, {X::F, Y::THREE});
-    expect_move_bad(src, {X::G, Y::TWO});
-    expect_move_bad(src, {X::H, Y::ONE});
-}
-
-TEST_F(KnightTest, MoveSW)
-{
-    Coordinate src{X::H, Y::EIGHT};
-    expect_move_bad(src, {X::G, Y::SEVEN});
-    expect_move_bad(src, {X::F, Y::SIX});
-    expect_move_bad(src, {X::E, Y::FIVE});
-    expect_move_bad(src, {X::D, Y::FOUR});
-    expect_move_bad(src, {X::C, Y::THREE});
-    expect_move_bad(src, {X::B, Y::TWO});
-    expect_move_bad(src, {X::A, Y::ONE});
+    expect_cannot_move_horizontal();
 }
 
 TEST_F(KnightTest, MoveSameSpot)
@@ -112,64 +26,154 @@ TEST_F(KnightTest, MoveSameSpot)
     expect_move_bad(pos, pos);
 }
 
-TEST_F(KnightTest, MoveSameSpotCorners)
+TEST_F(KnightTest, MoveLeftTwiceUpOnce)
 {
-    Coordinate bottom_left{X::A, Y::ONE};
-    Coordinate bottom_right{X::H, Y::ONE};
-    Coordinate top_left{X::A, Y::EIGHT};
-    Coordinate top_right{X::H, Y::EIGHT};
-    expect_move_bad(bottom_left, bottom_left);
-    expect_move_bad(bottom_right, bottom_right);
-    expect_move_bad(top_left, top_left);
-    expect_move_bad(top_right, top_right);
+    expect_move_ok(-2, 1);
 }
 
-TEST_F(KnightTest, MoveLongLeftShortUp)
+TEST_F(KnightTest, MoveLeftTwiceDownOnce)
 {
-    Coordinate src{X::E, Y::FOUR};
-    expect_move_ok(src, {X::C, Y::FIVE});
+    expect_move_ok(-2, -1);
 }
 
-TEST_F(KnightTest, MoveLongLefttShortDown)
+TEST_F(KnightTest, MoveRightTwiceUpOnce)
 {
-    Coordinate src{X::E, Y::FOUR};
-    expect_move_ok(src, {X::C, Y::THREE});
+    expect_move_ok(2, 1);
 }
 
-TEST_F(KnightTest, MoveLongRightShortUp)
+TEST_F(KnightTest, MoveRightTwiceDownOnce)
 {
-    Coordinate src{X::E, Y::FOUR};
-    expect_move_ok(src, {X::G, Y::FIVE});
+    expect_move_ok(2, -1);
 }
 
-TEST_F(KnightTest, MoveLongRightShortDown)
+TEST_F(KnightTest, MoveLeftOnceUpTwice)
 {
-    Coordinate src{X::E, Y::FOUR};
-    expect_move_ok(src, {X::G, Y::THREE});
+    expect_move_ok(-1, 2);
 }
 
-TEST_F(KnightTest, MoveShortLeftLongUp)
+TEST_F(KnightTest, MoveLeftOnceDownTwice)
 {
-    Coordinate src{X::E, Y::FOUR};
-    expect_move_ok(src, {X::D, Y::SIX});
+    expect_move_ok(-1, -2);
 }
 
-TEST_F(KnightTest, MoveShortLeftLongDown)
+TEST_F(KnightTest, MoveRightOnceUpTwice)
 {
-    Coordinate src{X::E, Y::FOUR};
-    expect_move_ok(src, {X::D, Y::TWO});
+    expect_move_ok(1, 2);
 }
 
-TEST_F(KnightTest, MoveShortRightLongUp)
+TEST_F(KnightTest, MoveRightOnceDownTwice)
 {
-    Coordinate src{X::E, Y::FOUR};
-    expect_move_ok(src, {X::F, Y::SIX});
+    expect_move_ok(1, -2);
 }
 
-TEST_F(KnightTest, MoveShortRightLongDown)
-{
-    Coordinate src{X::E, Y::FOUR};
-    expect_move_ok(src, {X::F, Y::TWO});
-}
+
+// TEST_F(KnightTest, MoveUp)
+// {
+//     expect_cannot_move_vertical();
+//     Coordinate src{X::A, Y::ONE};
+//     expect_move_bad(src, {X::A, Y::TWO});
+//     expect_move_bad(src, {X::A, Y::THREE});
+//     expect_move_bad(src, {X::A, Y::FOUR});
+//     expect_move_bad(src, {X::A, Y::FIVE});
+//     expect_move_bad(src, {X::A, Y::SIX});
+//     expect_move_bad(src, {X::A, Y::SEVEN});
+//     expect_move_bad(src, {X::A, Y::EIGHT});
+// }
+
+// TEST_F(KnightTest, MoveDown)
+// {
+//     Coordinate src{X::A, Y::EIGHT};
+//     expect_move_bad(src, {X::A, Y::SEVEN});
+//     expect_move_bad(src, {X::A, Y::SIX});
+//     expect_move_bad(src, {X::A, Y::FIVE});
+//     expect_move_bad(src, {X::A, Y::FOUR});
+//     expect_move_bad(src, {X::A, Y::THREE});
+//     expect_move_bad(src, {X::A, Y::TWO});
+//     expect_move_bad(src, {X::A, Y::ONE});
+// }
+
+// TEST_F(KnightTest, MoveLeft)
+// {
+//     Coordinate src{X::H, Y::ONE};
+//     expect_move_bad(src, {X::G, Y::ONE});
+//     expect_move_bad(src, {X::F, Y::ONE});
+//     expect_move_bad(src, {X::E, Y::ONE});
+//     expect_move_bad(src, {X::D, Y::ONE});
+//     expect_move_bad(src, {X::C, Y::ONE});
+//     expect_move_bad(src, {X::B, Y::ONE});
+//     expect_move_bad(src, {X::A, Y::ONE});
+// }
+
+// TEST_F(KnightTest, MoveRight)
+// {
+//     Coordinate src{X::A, Y::ONE};
+//     expect_move_bad(src, {X::B, Y::ONE});
+//     expect_move_bad(src, {X::C, Y::ONE});
+//     expect_move_bad(src, {X::D, Y::ONE});
+//     expect_move_bad(src, {X::E, Y::ONE});
+//     expect_move_bad(src, {X::F, Y::ONE});
+//     expect_move_bad(src, {X::G, Y::ONE});
+//     expect_move_bad(src, {X::H, Y::ONE});
+// }
+
+// TEST_F(KnightTest, MoveNE)
+// {
+//     Coordinate src{X::A, Y::ONE};
+//     expect_move_bad(src, {X::B, Y::TWO});
+//     expect_move_bad(src, {X::C, Y::THREE});
+//     expect_move_bad(src, {X::D, Y::FOUR});
+//     expect_move_bad(src, {X::E, Y::FIVE});
+//     expect_move_bad(src, {X::F, Y::SIX});
+//     expect_move_bad(src, {X::G, Y::SEVEN});
+//     expect_move_bad(src, {X::H, Y::EIGHT});
+// }
+
+// TEST_F(KnightTest, MoveNW)
+// {
+//     Coordinate src{X::H, Y::ONE};
+//     expect_move_bad(src, {X::G, Y::TWO});
+//     expect_move_bad(src, {X::F, Y::THREE});
+//     expect_move_bad(src, {X::E, Y::FOUR});
+//     expect_move_bad(src, {X::D, Y::FIVE});
+//     expect_move_bad(src, {X::C, Y::SIX});
+//     expect_move_bad(src, {X::B, Y::SEVEN});
+//     expect_move_bad(src, {X::A, Y::EIGHT});
+// }
+
+// TEST_F(KnightTest, MoveSE)
+// {
+//     Coordinate src{X::A, Y::EIGHT};
+//     expect_move_bad(src, {X::B, Y::SEVEN});
+//     expect_move_bad(src, {X::C, Y::SIX});
+//     expect_move_bad(src, {X::D, Y::FIVE});
+//     expect_move_bad(src, {X::E, Y::FOUR});
+//     expect_move_bad(src, {X::F, Y::THREE});
+//     expect_move_bad(src, {X::G, Y::TWO});
+//     expect_move_bad(src, {X::H, Y::ONE});
+// }
+
+// TEST_F(KnightTest, MoveSW)
+// {
+//     Coordinate src{X::H, Y::EIGHT};
+//     expect_move_bad(src, {X::G, Y::SEVEN});
+//     expect_move_bad(src, {X::F, Y::SIX});
+//     expect_move_bad(src, {X::E, Y::FIVE});
+//     expect_move_bad(src, {X::D, Y::FOUR});
+//     expect_move_bad(src, {X::C, Y::THREE});
+//     expect_move_bad(src, {X::B, Y::TWO});
+//     expect_move_bad(src, {X::A, Y::ONE});
+// }
+
+// TEST_F(KnightTest, MoveSameSpotCorners)
+// {
+//     Coordinate bottom_left{X::A, Y::ONE};
+//     Coordinate bottom_right{X::H, Y::ONE};
+//     Coordinate top_left{X::A, Y::EIGHT};
+//     Coordinate top_right{X::H, Y::EIGHT};
+//     expect_move_bad(bottom_left, bottom_left);
+//     expect_move_bad(bottom_right, bottom_right);
+//     expect_move_bad(top_left, top_left);
+//     expect_move_bad(top_right, top_right);
+// }
 
 }
--- test/fixtures/ChessPieceMovementTest.hpp
+++ test/fixtures/ChessPieceMovementTest.hpp
@@ -8,12 +8,56 @@ namespace fixtures {
 
 using namespace ChessGame;
 
+const uint8_t kMinX = static_cast<uint8_t>(X::A);
+const uint8_t kMaxX = static_cast<uint8_t>(X::H);
+const uint8_t kMinY = static_cast<uint8_t>(Y::ONE);
+const uint8_t kMaxY = static_cast<uint8_t>(Y::EIGHT);
+
 template <typename ChessPieceType, typename ChessPieceMovementSkillType>
 class ChessPieceMovementTest : public ::testing::Test {
 protected:
     void expect_move_ok(const Coordinate& src, const Coordinate& dst)
     {
         EXPECT_TRUE(move_from_to(src, dst));
+    }
+
+    void expect_move_ok(int x_offset, int y_offset)
+    {
+        for (uint8_t x = kMinX; x <= kMaxX; ++x) {
+            for (uint8_t y = kMinY; y <= kMaxY; ++y) {
+                int dst_x = x + x_offset;
+                int dst_y = y + y_offset;
+                bool is_dst_outside = dst_x < 1 || dst_x > 8 
+                    || dst_y < 1 || dst_y > 8;
+                if (is_dst_outside) {
+                    continue;
+                }
+                Coordinate src{static_cast<X>(x), static_cast<Y>(y)};
+                Coordinate dst{static_cast<X>(static_cast<uint8_t>(dst_x)),
+                    static_cast<Y>(static_cast<uint8_t>(dst_y))};
+                expect_move_ok(src, dst);
+            }
+        }
+    }
+
+    void expect_cannot_move_vertical()
+    {
+        for (uint8_t x = kMinX; x <= kMaxX; ++x) {
+            for (uint8_t y = kMinY; y <= kMaxY; ++y) {
+                // TODO: implement loop that expect_move_bad() on any movement up/down of any length
+                EXPECT_TRUE(false);
+            }
+        }
+    }
+
+    void expect_cannot_move_horizontal()
+    {
+        for (uint8_t x = kMinX; x <= kMaxX; ++x) {
+            for (uint8_t y = kMinY; y <= kMaxY; ++y) {
+                // TODO: implement loop that expect_move_bad() on any movement right/left of any length
+                EXPECT_TRUE(false);
+            }
+        }
     }
 
     void expect_move_bad(const Coordinate& src, const Coordinate& dst)
